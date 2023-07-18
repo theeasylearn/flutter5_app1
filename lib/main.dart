54bd1088e12545e99d203324000838c786605994
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-07-18 10:30:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  var app1 = new MyApp();
+  runApp(app1);
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+     return Material(
+       child: Center(
+         heightFactor: 1.25,
+         widthFactor: 1.25,
+         child: Container( //outer layout
+           height: 300,
+           width: 300,
+           color: Colors.yellow,
+           alignment: Alignment.center,
+           child: Container( //inner layout
+             height: 100,
+             width:100,
+             decoration: BoxDecoration(
+               color: Colors.red,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(50),
+                // shape: BoxShape.circle,
+             ),
+             alignment: Alignment.center,
+             child: Text("Example 2",textDirection: TextDirection.ltr,textScaleFactor: 2.0,),
+           ),
+         ),
+       ),
+     );
+  }
+}
+
